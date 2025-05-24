10c8efe6c28dabab0fa3fd54bbed252c5664b27a
Aman Shrivastav <amanshrivast21@gmail.com>
2025-05-24 15:04:13 +0530

    Create lambda-fuction-start-code.py

---
+++ lambda-fuction-start-code.py
@@ -0,0 +1,42 @@
+# Lambda function script for starting the server based on the snoozing tags
+
+import boto3
+import datetime
+import pytz
+
+ec2 = boto3.client('ec2')
+sns = boto3.client('sns')
+
+SNS_TOPIC_ARN = "arn:aws:sns:eu-west-1:619071349184:Server-start-alert"
+
+def lambda_handler(event, context):
+    now = datetime.datetime.now(pytz.utc).strftime("%H:%M")
+    affected_instances = []
+
+    filters = [{'Name': 'tag:Snoozing', 'Values': ['Yes']}]
+    instances = ec2.describe_instances(Filters=filters)
+
+    for reservation in instances['Reservations']:
+        for instance in reservation['Instances']:
+            instance_id = instance['InstanceId']
+            instance_type = instance['InstanceType']
+            
+            tags = {t['Key']: t['Value'] for t in instance.get('Tags', [])}
+            instance_name = tags.get("Name", "Unnamed")
+            start_time = tags.get("server-start-mon-friday", "")
+
+            if now == start_time and instance['State']['Name'] == 'stopped':
+                ec2.start_instances(InstanceIds=[instance_id])
+                affected_instances.append(f"Started: {instance_name} ({instance_id}, {instance_type})")
+
+    if affected_instances:
+        sns.publish(
+            TopicArn=SNS_TOPIC_ARN,
+            Message=f"Instances Started:\n" + "\n".join(affected_instances),
+            Subject="AWS EC2 Start Notification"
+        )
+
+    return {"status": "Start process completed", "affected_instances": affected_instances}
+
+
+
